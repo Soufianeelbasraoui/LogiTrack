b4afa865f26ea8d1bd5b6ffe9763f98f91db083c
soufiane <soufianeelbasraoui0@gmail.com>
2026-08-12 11:22:52 +0200

    update

--- src/main/java/org/example/logitrack/config/SecurityConfig.java
+++ src/main/java/org/example/logitrack/config/SecurityConfig.java
@@ -34,14 +34,12 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPointJwt))
