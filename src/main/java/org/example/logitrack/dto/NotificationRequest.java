b538d1a8468f68cfb7ebd2e72ebbbe519696e442
soufiane <soufianeelbasraoui0@gmail.com>
2026-09-02 10:42:27 +0200

    modifier NotificationRequest est StatutCommande,CommandeService

--- src/main/java/org/example/logitrack/dto/NotificationRequest.java
+++ src/main/java/org/example/logitrack/dto/NotificationRequest.java
@@ -1,4 +1,17 @@
 package org.example.logitrack.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationRequest {
+
+    private String message;
+
+    private String type;
+
+    private Long orderId;
 }
